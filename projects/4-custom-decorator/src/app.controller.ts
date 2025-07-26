a51aaef2db115bdc18ecfca720da8d4ddf5fe3d1
liren <327381633@qq.com>
2025-07-26 08:13:00 +0800

    update

--- projects/4-custom-decorator/src/app.controller.ts
+++ projects/4-custom-decorator/src/app.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AaaGuard } from './aaa.guard';
+import { Aaa } from './aaa.decorator';
+import { Bbb } from './bbb.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Aaa('admin-test')
+  @UseGuards(AaaGuard)
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Bbb('hello1', 'admin-hello1')
+  @UseGuards(AaaGuard)
+  getHello1(): string {
+    return 'this is hello1';
   }
 }
