be545c7f88a858ebdae5686de11c3cf9e5308326
liren <327381633@qq.com>
2025-07-28 07:58:21 +0800

    project9: a new project for uploading file practice

---
+++ projects/9-nest-multer-upload/src/app.controller.ts
@@ -0,0 +1,71 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UploadedFile,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
+import { AppService } from './app.service';
+import {
+  FileFieldsInterceptor,
+  FileInterceptor,
+  FilesInterceptor,
+} from '@nestjs/platform-express';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post('aaa')
+  @UseInterceptors(
+    FileInterceptor('aaa', {
+      dest: 'uploads',
+    }),
+  )
+  uploadFile(@UploadedFile() file: Express.Multer.File, @Body() body) {
+    console.log('body', body);
+    console.log('file', file);
+  }
+
+  @Post('bbb')
+  @UseInterceptors(
+    FilesInterceptor('bbb', 2, {
+      dest: 'uploads',
+    }),
+  )
+  uploadFiles(
+    @UploadedFiles() files: Array<Express.Multer.File>,
+    @Body() body,
+  ) {
+    console.log('body', body);
+    console.log('files', files);
+  }
+
+  @Post('ccc')
+  @UseInterceptors(
+    FileFieldsInterceptor(
+      [
+        { name: 'aaa', maxCount: 2 },
+        { name: 'bbb', maxCount: 2 },
+      ],
+      {
+        dest: 'uploads',
+      },
+    ),
+  )
+  uploadFileFields(
+    @UploadedFiles()
+    files: { aaa?: Express.Multer.File[]; bbb?: Express.Multer.File[] },
+    @Body() body,
+  ) {
+    console.log('body', body);
+    console.log('files', files);
+  }
+}
